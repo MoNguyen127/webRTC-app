4b56f8b9085529780244f3dc942b7068ab963583
MoNguyen127 <70472268+MoNguyen127@users.noreply.github.com>
2024-06-04 09:56:19 +0700

    Update face-recognize.js

--- js/face-recognize.js
+++ js/face-recognize.js
@@ -1,7 +1,7 @@
 // script.js
 
 async function setup() {
-    await faceapi.nets.tinyFaceDetector.loadFromUri('models');
+    await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
     await faceapi.nets.faceLandmark68Net.loadFromUri('models');
     await faceapi.nets.faceRecognitionNet.loadFromUri('models');
     await faceapi.nets.faceExpressionNet.loadFromUri('models');
